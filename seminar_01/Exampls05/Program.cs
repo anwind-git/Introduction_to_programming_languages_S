2705a50538f96b4de6d0fa0f0f36ebbf5b059069
anwind-git <work-git@yandex.ru>
2022-09-18 10:50:26 +0300

    Добавил Exampls05

---
+++ seminar_01/Exampls05/Program.cs
@@ -0,0 +1,37 @@
+﻿// Программа которая будет выдавать название дня недели по заданному номеру
+
+Console.Write("Введите номер дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (num == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (num == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (num == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (num == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (num == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (num == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Ошибка ввода");
+}
