aa3a56264d2288e9ee104859075d49ea8d247260
anwind-git <work-git@yandex.ru>
2022-09-18 11:32:54 +0300

    Добавил Exampls07

---
+++ seminar_01/Exampls07/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает одно число 
+// (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+num1 = Math.Abs(num1);
+int num2 = -num1;
+while (num2 <= num1)
+{
+    Console.Write(num2 + ", ");
+    num2 = num2 + 1;
+}
+Console.WriteLine();
