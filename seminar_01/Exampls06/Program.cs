3c3807c6877874f678a8dd3dabc7ad66a27794cc
anwind-git <work-git@yandex.ru>
2022-09-18 11:17:45 +0300

    Добавил Exampls06

---
+++ seminar_01/Exampls06/Program.cs
@@ -0,0 +1,10 @@
+﻿//функция возведения числа в квадрат
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Power(int x)
+{
+    return x * x;
+}
+Console.WriteLine(Power(num));
