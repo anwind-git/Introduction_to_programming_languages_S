237f5e6f57b0aecdf7fbae91fee72e47a756e8c1
anwind-git <work-git@yandex.ru>
2022-09-18 09:41:26 +0300

    Добавил Exampls03

---
+++ seminar_01/Exampls03/Program.cs
@@ -0,0 +1,8 @@
+﻿int time = 11;
+while(time < 22)
+{
+    time++;
+    Console.WriteLine("Еще можно смотреть мультикию Время: " + time);
+}
+Console.WriteLine("Пора спать");
+Console.WriteLine(time);
