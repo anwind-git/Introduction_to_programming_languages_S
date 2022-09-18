bcac987f7634600e47799ade5ab8d50846d92cfe
anwind-git <work-git@yandex.ru>
2022-09-18 10:23:43 +0300

    Добавил Exampls04

---
+++ seminar_01/Exampls04/Program.cs
@@ -0,0 +1,15 @@
+﻿// являеться ли первое число квадратом второго
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 == num2*num2)
+{
+    Console.WriteLine("Первое число являеться квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не являеться квадратом второго");
+}
