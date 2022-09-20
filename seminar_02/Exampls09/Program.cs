f86ed34ad29f49d1c1250ec0775587416ef57469
anwind-git <work-git@yandex.ru>
2022-09-20 18:42:26 +0300

    выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа

---
+++ seminar_02/Exampls09/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Random random = new Random();
+int number = random.Next(10, 100);
+Console.WriteLine(number);
+
+int tens = number / 10;
+int ones = number % 10;
+
+if (tens > ones)
+{
+    Console.WriteLine(tens);
+}
+else
+{
+    Console.WriteLine(ones);
+}
