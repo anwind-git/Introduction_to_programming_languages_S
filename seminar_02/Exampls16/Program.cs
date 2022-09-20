34120261245492d804a67fe367ddc2cf1cf93c60
anwind-git <work-git@yandex.ru>
2022-09-20 20:47:25 +0300

    Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого

---
+++ seminar_02/Exampls16/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num2 == num2 * num2 || num2 == num1 * num1)
+    Console.WriteLine("да");
+else
+     Console.WriteLine("нет");
