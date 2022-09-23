6645a53fdb706f08b8ee68f52b5b27dd48fb851c
anwind-git <work-git@yandex.ru>
2022-09-23 18:22:43 +0300

    принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N

---
+++ seminar_03/Exampls22/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу 
+// квадратов чисел от 1 до N
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+double summa = 1;
+
+while (i <= N)
+{
+    summa = Math.Pow(i, 2);
+    Console.WriteLine($"Квадрат числа {i}: ({i} * {i}) = {summa}");
+    i++;
+}
