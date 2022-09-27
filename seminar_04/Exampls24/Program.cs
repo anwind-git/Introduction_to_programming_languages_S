1613dc257a1f76b3e761499e1c0b5ffdf9fde3f4
anwind-git <work-git@yandex.ru>
2022-09-27 18:01:10 +0300

    принимает на вход число (А) и выдаёт сумму чисел от 1 до А

---
+++ seminar_04/Exampls24/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А
+
+Console.Write("Введите число A: ");
+int number_a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+for (int i = 1; i <= number_a; i++)
+    sum = sum + i;
+
+Console.WriteLine(sum);
