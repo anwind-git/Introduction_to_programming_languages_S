1baff003ccf74558cb6da6f9bdb8df8d582fed99
anwind-git <work-git@yandex.ru>
2022-09-27 18:15:00 +0300

    принимает на вход число и выдаёт количество цифр в числе

---
+++ seminar_04/Exampls26/Program.cs
@@ -0,0 +1,10 @@
+﻿// принимает на вход число и выдаёт количество цифр в числе
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string Length = Convert.ToString(number);
+
+int col = Length.Length;
+
+Console.WriteLine(col);
