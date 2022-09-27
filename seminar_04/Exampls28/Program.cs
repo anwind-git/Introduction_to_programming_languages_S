6de8ff7ae0a6ac043317048a9cecf4659b0b2112
anwind-git <work-git@yandex.ru>
2022-09-27 18:23:37 +0300

    принимает на вход число N и выдаёт произведение чисел от 1 до N

---
+++ seminar_04/Exampls28/Program.cs
@@ -0,0 +1,12 @@
+﻿// принимает на вход число N и выдаёт произведение чисел от 1 до N
+
+Console.Write("Введите число N: ");
+int number_n = Convert.ToInt32(Console.ReadLine());
+int sum = 1;
+
+for (int i = 1; i <= number_n; i++)
+{
+    sum = sum * i;
+}
+
+Console.WriteLine(sum);
