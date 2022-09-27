6022e0adc44e682c18c76fa9e7bbce856d8a74b4
anwind-git <work-git@yandex.ru>
2022-09-27 18:41:55 +0300

    выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке

---
+++ seminar_04/Exampls30/Program.cs
@@ -0,0 +1,12 @@
+﻿// выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке
+
+int[] array = new int[8];
+
+Random random = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(0, 2);
+}
+string arr = string.Join(", ", array);
+Console.Write(arr);
