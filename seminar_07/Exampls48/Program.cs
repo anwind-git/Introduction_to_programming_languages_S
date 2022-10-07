6f6de36768b573f1ee234642ea62878ec365aab0
anwind-git <work-git@yandex.ru>
2022-10-07 10:36:04 +0300

    двумерный массив размера m на n, каждый элемент в массиве находится по формуле: A = m+n. Выведите полученный массив на экран.

---
+++ seminar_07/Exampls48/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: A = m+n. 
+// Выведите полученный массив на экран.
+
+void Zadaha46()
+{
+    int rows = 3;
+    int colunms = 4;
+    Console.WriteLine($"Массив размера {rows}x{colunms}");
+
+    int[,] numbers = new int[rows, colunms];
+
+    FillArray(numbers);
+    PrintArray(numbers);
+}
+
+void FillArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int colunms = numbers.GetLength(1);
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < colunms; j++)
+        {
+            numbers[i, j] = i + j;
+        }
+    }
+}
+void PrintArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int colunms = numbers.GetLength(1);
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < colunms; j++)
+        {
+            Console.Write(numbers[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Zadaha46();
